e0a945e1027f7748e60311d3bb8f0f1c789d6f1b
Justin Page <justin.page89@gmail.com>
2020-08-15 04:30:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,13 +14,14 @@ introduction Justin
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+introduction_with_language Justin, Elixer
 
 
 def introduction_with_language_optional (name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-
+introduction_with_language_optional Justin
 
 
 
